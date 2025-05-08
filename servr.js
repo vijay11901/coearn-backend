5d14ceb7cfc4b17899a2269dfd4f5044cdfbd2c5
vijay11901 <vijaymeena827864@gmail.com>
2025-05-08 10:31:37 +0530

    Fix: Use dynamic PORT for Render

--- servr.js
+++ servr.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
   res.send('Server running from Render');
 });
 
+// Render ke liye port ENV se lena zaruri hai
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is live on port ${PORT}`);
